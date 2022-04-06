b9a65256d6fef80202cb715e23294b3c6c80de91
Jose Silva <josesilva8925@gmail.com>
2022-04-05 21:44:05 -0500

    Se genera html y se crea calculo de áreas de figuras geometricas y se manda a llamr dede html

--- figuras.js
+++ figuras.js
@@ -1,23 +1,78 @@
 //Figura Cuadrado//
 
-console.group("Cuadrado");
-  const ladoCuadrado= 5;
-  console.log("la medida de lado del cuadrado es " + ladoCuadrado + " cm" );
-  const perimetroCuadrado = ladoCuadrado *4;
-  console.log("El perimetro del cuadrado es: "+ perimetroCuadrado+" cm");
-  const areaCuadrado = ladoCuadrado * ladoCuadrado;
-  console.log("El área del cuadrado es: "+ areaCuadrado + " cm^2")
-console.groupEnd();
+function perimetroCuadrado(lado){
+  return lado*4;
+}
 
-console.group("Triangulo");
-  const lado1= 6;
-  const lado2=6;
-  const base= 5;
-  const altura= 7;
-  console.log("La medida del lado1 del triángulo es: "+lado1+ " el lado 2 es igual a: "+lado2+" la base es igual a: "+base+" la altura es igual a: "+altura);
-  const perimetroTriangulo= lado1 + lado2 + base;
-  console.log("El perimetro del triangulo es igual a: "+perimetroTriangulo);
-  const areaTriangulo= (base * altura )/2;
-  console.log("El área del triángilo es: "+ areaTriangulo);
+function areaCuadrado(lado){
+  return lado * lado;
+}
 
-console.groupEnd();
+//Figura Triángulo
+
+function perimetroTriangulo(lado1,lado2,base){
+  return lado1+lado2+base;
+}
+
+function areaTriangulo(base, altura){
+  return (base * altura) / 2;
+}
+
+//Figura Circulo
+
+function perimetroCirculo(radio){
+   return Math.PI * (radio * 2);
+}
+
+function areaCirculo(radio){
+  return (radio * radio) * Math.PI;
+}
+
+//Integración con html
+// Calculando área y perimetro del cuadrado
+function calcularArea(){
+  var input = document.getElementById("valor").value;
+  alert("El área es: "+ areaCuadrado(input));
+}
+
+function calcularPerimetro(){
+  var input= document.getElementById("valor").value;
+  alert("El perimetro es: " + perimetroCuadrado(input));
+}
+
+//Calculando área y perimetro del triángulo
+
+
+
+function calcularPerimetroTriangulo(){
+  var base= document.getElementById("base").value;
+  var ladoA= document.getElementById("lado1").value;
+  var ladoB= document.getElementById("lado2").value;  
+  var resultado= perimetroTriangulo(parseInt(ladoA), parseInt(ladoB), parseInt(base));
+  alert("El perimetro del triángulo " + resultado);
+   console.log(typeof(resultado));
+
+}
+
+function calcularAreaTriangulo(){
+  var base= document.getElementById("base").value;
+  var altura= document.getElementById("altura").value;
+  alert("El área del triángulo es: " + areaTriangulo(base,altura));
+}
+
+//Calculando área y perimetro del Circulo
+
+
+function calcularPerimetroCirculo(){
+  var radio= document.getElementById("radio").value;
+  alert("El Perimetro del Circulo es: " + perimetroCirculo(radio));
+}
+
+function calcularAreaCirculo(){
+  var radio= document.getElementById("radio").value;
+  alert("El área del Circulo es: " + areaCirculo(radio));
+}
+
+function alturaIsoseles(lado,base){
+  return Math.sqrt((lado*lado)-((base*base)/4));
+}
